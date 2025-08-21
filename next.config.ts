751adf504e94103bd483e0551f641ce0f73215d8
Jyoti Rajput <jyoti865072@gmail.com>
2025-08-21 10:01:58 +0000

    In videos ki niche photo load Nahi ho rhi ha

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i3.ytimg.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
